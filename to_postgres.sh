d790ee06e5a9b30802ea558c4f7dbcef82f65890
ziggy <ziggy@elephant-bird.net>
2017-05-18 09:11:43 -0400

    use var instead of source

--- to_postgres.sh
+++ to_postgres.sh
@@ -5,10 +5,11 @@ set -eu
 source ./env.sh
 
 if [[ -d venv ]];then
-    source venv/bin/activate
+    _py=venv/bin/python3
+else
+    _py=python3
 fi
-
 
 execute_sql schema.sql
 
-python3 xls_to_csv.py $1 - | execute_sql_cmd "COPY dof_sales(Borough,  Neighborhood,  BuildingClassCategory,  TaxClassAtPresent,  Block,  Lot,  EaseMent,  BuildingClassAtPresent,  Address,  ApartmentNumber,  ZipCode,  ResidentialUnits,  CommercialUnits, TotalUnits, LandSquareFeet,  GrossSquareFeet,  YearBuilt,  TaxClassAtTimeOfSale,  BuildingClassAtTimeOfSale,  SalePrice, SaleDate, bbl) FROM STDIN WITH (FORMAT CSV, HEADER TRUE);"
+$_py xls_to_csv.py $1 - | execute_sql_cmd "COPY dof_sales(Borough,  Neighborhood,  BuildingClassCategory,  TaxClassAtPresent,  Block,  Lot,  EaseMent,  BuildingClassAtPresent,  Address,  ApartmentNumber,  ZipCode,  ResidentialUnits,  CommercialUnits, TotalUnits, LandSquareFeet,  GrossSquareFeet,  YearBuilt,  TaxClassAtTimeOfSale,  BuildingClassAtTimeOfSale,  SalePrice, SaleDate, bbl) FROM STDIN WITH (FORMAT CSV, HEADER TRUE);"
